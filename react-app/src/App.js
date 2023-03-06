784a25cbf0f67456ba764424a46894b5af57ef54
hhcheung0 <80027389+hhcheung0@users.noreply.github.com>
2023-03-06 22:13:01 +0800

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import './index.scss';
+import {BrowserRouter,  Routes,  Route} from 'react-router-dom';
+import Login from './Login';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+          <Routes>
+              <Route path="/login" element={<Login />}/>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
