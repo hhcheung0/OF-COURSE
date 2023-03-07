742ee5d586e11fc19d5ea3e4038eceadd95495da
dltnwlsz <suzielee0727@gmail.com>
2023-03-07 19:14:58 +0800

    data field/variable name change

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect('mongodb+srv://admin:admin@cluster0.brap3r8.mongodb.net/test')
 const db = mongoose.connection;
 const Schema = mongoose.Schema;
 
@@ -22,21 +23,21 @@ db.once('open', () => {
     console.log('Database is open...')
 
     const UserSchema = Schema({
-        name: { type: String, required: true, unique: true },
-        userid: { type: Number, required: true, unique: true },
-        password: { type: String, required: true },
-        accessright: { type: Boolean, required: true },
-        enrolledcourseid: { type: Array },
-        maxCredit: { type: Number },
-        passedcourseid: { type: Array },
-        shoppingcartcourseid: { type: Array }
+        Username: { type: String, required: true, unique: true },
+        UserID: { type: Number, required: true, unique: true },
+        Password: { type: String, required: true },
+        Accessright: { type: Boolean, required: true },
+        EnrolledcourseID: { type: Array },
+        MaxCredit: { type: Number },
+        Passedcourseid: { type: Array },
+        Shoppingcartcourseid: { type: Array }
       });
     const User = mongoose.model('User', UserSchema);
 
     app.post('/signup', (req, res) => {
-        const { name, password, confirmpassword } = req.body;
+        const { Username, Password, Confirmpassword } = req.body;
 
-        if (password !== confirmpassword) {
+        if (Password !== Confirmpassword) {
             return res.status(400).send('Passwords do not match');
         }
 
@@ -50,7 +51,7 @@ db.once('open', () => {
                 return res.status(500).send('Internal server error');
             }
 
-            bcrypt.hash(password, 10, (err, hash) => {
+            bcrypt.hash(Password, 10, (err, hash) => {
                 if (err) {
                     console.error(err);
                     return res.status(500).send('Internal server error');
@@ -59,14 +60,14 @@ db.once('open', () => {
                 const newuserid = user.length > 0 ? user[0].userid + 1 : 1;
 
                 const newUser = new User({
-                    name: name,
-                    password: hash,
-                    userid: newuserid,
-                    accessright: 0,
-                    enrolledcourseid: [],
-                    maxCredit: 18,
-                    passedcourseid: [],
-                    shoppingcartcourseid: []
+                    Username: Username,
+                    Password: hash,
+                    Userid: newuserid,
+                    Accessright: 0,
+                    Enrolledcourseid: [],
+                    MaxCredit: 18,
+                    Passedcourseid: [],
+                    Shoppingcartcourseid: []
                 });
 
                 newUser.save((err) => {
@@ -83,13 +84,13 @@ db.once('open', () => {
     });
 
     app.post('/login', (req, res) => {
-        const { name, password } = req.body;
+        const { Username, Password } = req.body;
 
-        User.findOne({ name: name }, (err, user) => {
+        User.findOne({ Username: Username }, (err, user) => {
             if (err) { return res.status(500).send(err); }
             if (!user) { return res.status(401).send('Incorrect username or password'); }
             
-            bcrypt.compare(password, user.password, (err, isMatch) => {
+            bcrypt.compare(Password, user.Password, (err, isMatch) => {
                 if (err) { return res.status(500).send(err); }
                 if (isMatch) {
                     res.status(200).send('Login successful');
@@ -104,7 +105,7 @@ db.once('open', () => {
 
 })
 
-var port = 3001
+var port = 3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
