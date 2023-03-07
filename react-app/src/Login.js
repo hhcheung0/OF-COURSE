dc9d05a18b420311de2db06df9ad5f001f872f4a
hhcheung0 <1155144254@link.cuhk.edu.hk>
2023-03-07 12:38:40 +0800

    login changes

--- react-app/src/Login.js
+++ react-app/src/Login.js
@@ -1,18 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+//import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
+    const [name, setname] = useState("");
     const [password, setPassword] = useState("");
-    const [errorMessages, setErrorMessages] = useState({});
-    const navigate = useNavigate();
-
-    // User login error msg
-    const errors = {
-        loginerror: "Invalid username or password. Please try again.",
-        username: "Invalid username",
-        password: "Invalid password"
-    };
+    
+    //const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,42 +13,38 @@ const Login = () => {
         fetch('/login', {
             method: 'POST',
             headers: {'Content-type' : 'application/json'},
-            body: JSON.stringify({name : username})
+            body: JSON.stringify({
+                name : name,
+                password: password
+             })
         })
         .then(res => {
-            return res.text();
-        })
-        .then(data => {          
-            if (password === data) {
-                navigate("/");
+            if(res.ok){
+                console.log("Login successful");
             }else{
-                setErrorMessages({ name: "loginerror", message: errors.loginerror });
-            };
+                console.error(res.statusText);
+            }
         })
+        .catch(error => console.log(error));
     };
-
-    const renderErrorMessage = (name) => name === errorMessages.name && (
-        <div className="login-error">{errorMessages.message}</div>
-  );
 
     return(
         <div>
             <h2>Login Page</h2>
             <div className="container">
                 <form onSubmit={handleSubmit}>
-                    <label name="username"><b>Username </b></label>
-                    <input type="text" placeholder="Enter your username" name="username" onChange={(e) => setUsername(e.target.value)} required />
+                    <label name="name"><b>Username </b></label>
+                    <input type="text" placeholder="Enter your username" name="name" value={name} onChange={(e) => setname(e.target.value)} required />
                     
                     <br /><br />
 
                     <label name="password"><b>Password </b></label>
-                    <input type="password" placeholder="Enter your password" name="password" onChange={(e) => setPassword(e.target.value)} required />
+                    <input type="password" placeholder="Enter your password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
 
                     <br /><br />
                     
                     <button type="submit">Login</button>
                     <p>Not registered? <a href="/">Create an account</a></p>   
-                    {renderErrorMessage("loginerror")}
                 </form>
             </div>
 
--- server/server.js
+++ server/server.js
@@ -3,33 +3,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bcrypt = require('bcrypt')
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// This module is for parsing the content in a request body (installed with npm)
-const bodyParser = require('body-parser');
-// Use parser to obtain the content in the body of a request
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
-
-app.use(cors());
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI)
-const db = mongoose.connection
+const db = mongoose.connection;
 const Schema = mongoose.Schema;
 
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
     console.log('Database is open...')
-    
-    /*app.get('/', (req, res) => {
-        res.send('Hello World!')
-    })*/
 
     const UserSchema = Schema({
         name: { type: String, required: true, unique: true },
@@ -45,24 +35,26 @@ db.once('open', () => {
 
   
     app.post('/login', (req, res) => {
-        User
-        .findOne({name: req.body.name})
-        .exec(function(err, user) {
-            if (err)
-                console.log('Get login user error: ' + err);
-            else {
-                if (user == null){
-                    res.send("");
-                }else{
-                    res.send(user.password);
+        const { name, password } = req.body;
+
+        User.findOne({ name: name }, (err, user) => {
+            if (err) { return res.status(500).send(err); }
+            if (!user) { return res.status(401).send('Incorrect username or password'); }
+            
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) { return res.status(500).send(err); }
+                if (isMatch) {
+                    res.status(200).send('Login successful');
+                } else {
+                    return res.status(401).send('Incorrect username or password');
                 }
-            }
-        })
+            });
+        });
     });  
 
 })
 
-var port = process.env.PORT || 3000
+var port = 3001
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
