abd09446c5e5ebecb7db7edfe1a280a64ef4b084
James Chu <jameschutszchim131@gmail.com>
2023-03-16 13:35:49 +0800

    transformMongo updatev2

---
+++ server/Misc/transformMongo.js
@@ -0,0 +1,49 @@
+const {MongoClient} = require('mongoose');
+require('dotenv').config();
+
+async function main(){
+    const uri = process.env.MONGODB_URI;
+    const client = new MongoClient(uri);
+    try{
+        await client.connect();
+        //await listDatabases(client);
+        await updateDataTypes(client);
+
+    } catch(e){
+        console.error(e);
+    }finally{
+        //await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases (client){
+    const dbList = await client.db().admin().listDatabases();
+    
+    console.log("Databases");
+    dbList.databases.forEach(db =>{
+        console.log(`- ${db.name}`);
+    })
+}
+
+async function updateDataTypes(client) {
+    client.db("test").collection("courses").updateMany({},{$pull: {"courseTime":""}});
+    client.db("test").collection("courses").updateMany({},{$pull: {"enrolledID":""}});
+    client.db("test").collection("courses").updateMany({},{$pull: {"prerequisiteCourseID":""}});
+    client.db("test").collection("courses").updateMany({},{$pull: {"forbiddenCourseID":""}});
+    //client.db("test").collection("courses").updateMany({},{$set: {"comment.$[element].userID":null}},{arrayFilters: [{"element":0}]});
+    //client.db("test").collection("testingaa").updateMany({'enrolledID':""},{$set: {"enrolledID.$[element]":null}},{arrayFilters: [ {"element":""}]});
+    //client.db("test").collection("courses").updateMany({'enrolledID':""},{$set: {"enrolledID.$[element]":null},{arrayFilters: [ {"element":""}]})
+    client.db("test").collection("courses").updateMany(
+        {"tutorialInfo.tutorialTime": ""},
+        { $pull: { tutorialInfo: { tutorialTime: "" }} },
+        { multi: true }
+      );
+      client.db("test").collection("courses").updateMany({},{$pull: {"tutorialInfo.$[].enrolledID":""}});  
+      //client.db("test").collection("courses").updateMany({},{$addFields:{"comment.0.date":{$toDate: "$comment.0.date"}}});
+    
+      
+
+
+}
