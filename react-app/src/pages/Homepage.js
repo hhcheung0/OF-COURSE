4b127d06aa295ee06912b0a5205e553ec1363c14
hhcheung0 <1155144254@link.cuhk.edu.hk>
2023-03-30 18:01:41 +0800

    Add full-page-tab to Homepage.js

--- react-app/src/pages/Homepage.js
+++ react-app/src/pages/Homepage.js
@@ -1,3 +1,46 @@
+import React, { useState } from "react";
+
 const Homepage = () => {
-    
-}
+    const [activeTab, setActiveTab] = useState("courseTab");
+  
+    return (
+      <div id="full-page-tab">
+        <div className="tab-buttons">
+          <button
+            className={activeTab === "courseTab" ? "active" : ""}
+            onClick={() => setActiveTab("courseTab")}
+          >
+            Courses
+          </button>
+          <button
+            className={activeTab === "timetableTab" ? "active" : ""}
+            onClick={() => setActiveTab("timetableTab")}
+          >
+            Timetable
+          </button>
+        </div>
+        <div className="tab-content">
+          {activeTab === "courseTab" && <CourseTab />}
+          {activeTab === "timetableTab" && <TimetableTab />}
+        </div>
+      </div>
+    );
+};
+
+function CourseTab() {
+    return (
+      <div>
+        <h1>Course</h1>
+      </div>
+    );
+  }
+  
+  function TimetableTab() {
+    return (
+      <div>
+        <h1>Timetable</h1>
+      </div>
+    );
+  }
+
+export default Homepage;
