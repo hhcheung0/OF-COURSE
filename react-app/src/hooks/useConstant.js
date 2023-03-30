e0ace50df43f6dfdff54b667459dfb3475c2cf70
Hinson <newspaperhinson@gmail.com>
2023-03-30 21:03:36 +0800

    added weekday filter function

--- react-app/src/hooks/useConstant.js
+++ react-app/src/hooks/useConstant.js
@@ -16,22 +16,30 @@ const useConstant = () => {
         'Spanish',          'Thai',
         'Theology',         'Translation'
       ]
-    const classStartingTimeList = [
-        '08:30',
-        '09:30',
-        '10:30',
-        '11:30',
-        '12:30',
-        '13:30',
-        '14:30',
-        '15:30',
-        '16:30',
-        '17:30'
+    const classTimeList = [
+        '08:30-09:15',
+        '09:30-10:15',
+        '10:30-11:15',
+        '11:30-12:15',
+        '12:30-13:15',
+        '13:30-14:15',
+        '14:30-15:15',
+        '15:30-16:15',
+        '16:30-17:15',
+        '17:30-18:15'
+    ]
+    const weekdayList = [
+        ['M', 'Monday'],
+        ['T', 'Tuesday'],
+        ['W', 'Wednesday'],
+        ['H', 'Thursday'],
+        ['F', 'Friday']
     ]
 
     return {
         departmentList,
-        classStartingTimeList
+        classTimeList,
+        weekdayList
     }
 }
 
