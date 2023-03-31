47373eee94f295c7dea1ce7c098d1815d846f1b1
hhcheung0 <1155144254@link.cuhk.edu.hk>
2023-03-31 17:42:06 +0800

    Added Homepage timetable

--- react-app/src/components/CourseTab.js
+++ react-app/src/components/CourseTab.js
@@ -1,150 +1,8 @@
 import { BsArrowDown, BsArrowUp, BsTrash3} from 'react-icons/bs';
 
 const CourseTab = () => {
-    const EnrolledTable = () => {
-        return(
-            <>
-            <h2>Enrolled Courses</h2>
-            <table id='homepage-table'>
-                <thead>
-                <tr>
-                    <th>Course ID</th>
-                    <th>Course Name</th>
-                    <th></th>
-                    <th>Time</th>
-                    <th>Location</th>
-                    <th>Credit</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>BIOL2410</td>
-                    <td>Applications of Biology</td>
-                    <td>LEC</td>
-                    <td>Tue 8:30 – 10:15</td>
-                    <td>YIA Room 1</td>
-                    <td>3</td>
-                    <td><button><BsTrash3 /> Drop</button></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td>T01</td>
-                    <td>Fri 9:30 – 10:15</td>
-                    <td>YIA Room 1</td>
-                    <td></td>
-                </tr>
-                </tbody>
-            </table>
-            </>
-        )
-    }
-        
-    const ShoppingCartTable = () => {
-        return(
-        <>
-            <div id="ShoppingCart" style={{display: 'flex'}}>
-                <h2 style={{flex: 8}}>Shopping Cart</h2>
-                <button style={{flex: 1}}>Enroll</button>
-            </div>
-            <table id ="homepage-table">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Course ID</th>
-                    <th>Course Name</th>
-                    <th></th>
-                    <th>Time</th>
-                    <th>Location</th>
-                    <th>Credit</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><input type="checkbox" /></td>
-                    <td>MGNT1040</td>
-                    <td>Introduction to Management 1</td>
-                    <td>LEC</td>
-                    <td>Tue 14:30–16:15</td>
-                    <td>MMW Room 1</td>
-                    <td>2</td>
-                    <td><button><BsTrash3 /> Delete</button></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>T01</td>
-                    <td>Fri 9:30 – 10:15</td>
-                    <td>YIA Room 1</td>
-                    <td></td>
-                </tr>
-            </tbody>
-            </table>
-        </>
-        )
-    }
-        
-    const CompletedTable = () => {
-        return(
-        <>
-            <div style={{display: 'flex'}}> 
-            <h2>Completed Courses </h2> 
-            <p id="homepage-course-gpa">GPA 4.00/4.30</p> 
-            </div>
-            <table id ="homepage-table">
-            <thead>
-                <tr>
-                <th>Course ID</th>
-                <th>Course Name</th>
-                <th>Semester</th>
-                <th>Credit</th>
-                <th>Grade</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>GEOG1040</td>
-                <td>Recent History of Geography</td>
-                <td>2021-2022 Sem 1</td>
-                <td>3</td>
-                <td>A</td>
-                </tr>
-            </tbody>
-            </table>
-        </>
-        )
-    }
-        
-    const SwapCourse = () => {
-        return(
-        <div id="swap-container"> 
-            <h2>Swap Courses</h2>
-            <p>From:</p>
-            <select>
-            <option value="">MGNT1040</option>
-            <option value="">MGNT1023</option>
-            </select>
-    
-            <br/><br/>
-            <BsArrowDown size={70}/>&ensp;<BsArrowUp size={70}/>
-            <br/>
-        
-            <p>To:</p>
-            <select>
-            <option value="">MGNT1041</option>
-            <option value="">BIOL2410</option>
-            </select>
-            <br/><br/>
-            <button>Swap</button>
-        </div>
-        )
-    }
-
     return (
-        <div id="homepage-course">
+        <div id="homepage-course-tab">
             <div id="homepage-course-up">
             <div id = "homepage-course-upperleft">
                 <EnrolledTable />
@@ -165,6 +23,149 @@ const CourseTab = () => {
         </div>
     );
 };
+
+const EnrolledTable = () => {
+    return(
+        <>
+        <h2>Enrolled Courses</h2>
+        <table id='homepage-table'>
+            <thead>
+            <tr>
+                <th>Course ID</th>
+                <th>Course Name</th>
+                <th></th>
+                <th>Time</th>
+                <th>Location</th>
+                <th>Credit</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>BIOL2410</td>
+                <td>Applications of Biology</td>
+                <td>LEC</td>
+                <td>Tue 8:30 – 10:15</td>
+                <td>YIA Room 1</td>
+                <td>3</td>
+                <td><button><BsTrash3 /> Drop</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td>T01</td>
+                <td>Fri 9:30 – 10:15</td>
+                <td>YIA Room 1</td>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+        </>
+    )
+}
+    
+const ShoppingCartTable = () => {
+    return(
+    <>
+        <div id="ShoppingCart">
+            <h2>Shopping Cart</h2>
+            <button>Enroll</button>
+        </div>
+        <table id ="homepage-table">
+        <thead>
+            <tr>
+                <th></th>
+                <th>Course ID</th>
+                <th>Course Name</th>
+                <th></th>
+                <th>Time</th>
+                <th>Location</th>
+                <th>Credit</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><input type="checkbox" /></td>
+                <td>MGNT1040</td>
+                <td>Introduction to Management 1</td>
+                <td>LEC</td>
+                <td>Tue 14:30–16:15</td>
+                <td>MMW Room 1</td>
+                <td>2</td>
+                <td><button><BsTrash3 /> Delete</button></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>T01</td>
+                <td>Fri 9:30 – 10:15</td>
+                <td>YIA Room 1</td>
+                <td></td>
+            </tr>
+        </tbody>
+        </table>
+    </>
+    )
+}
+    
+const CompletedTable = () => {
+    return(
+    <>
+        <div id='CompletedTable'> 
+            <h2>Completed Courses </h2> 
+            <p>GPA 4.00/4.30</p> 
+        </div>
+        <table id ="homepage-table">
+        <thead>
+            <tr>
+            <th>Course ID</th>
+            <th>Course Name</th>
+            <th>Semester</th>
+            <th>Credit</th>
+            <th>Grade</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>GEOG1040</td>
+            <td>Recent History of Geography</td>
+            <td>2021-2022 Sem 1</td>
+            <td>3</td>
+            <td>A</td>
+            </tr>
+        </tbody>
+        </table>
+    </>
+    )
+}
+    
+const SwapCourse = () => {
+    return(
+    <div id="swap-container"> 
+        <h2>Swap Courses</h2>
+        <p>From:</p>
+        <select>
+        <option value="">MGNT1040</option>
+        <option value="">MGNT1023</option>
+        </select>
+
+        <br/><br/>
+        <BsArrowDown size={70}/>&ensp;<BsArrowUp size={70}/>
+        <br/>
+    
+        <p>To:</p>
+        <select>
+        <option value="">MGNT1041</option>
+        <option value="">BIOL2410</option>
+        </select>
+        <br/><br/>
+        <button>Swap</button>
+    </div>
+    )
+}
+
 
 export default CourseTab
 
--- react-app/src/components/TimetableTab.js
+++ react-app/src/components/TimetableTab.js
@@ -1,9 +1,117 @@
+import React, { useState, useEffect } from 'react';
+
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+const timeSlots = [
+    { startTime: '08:30', endTime: '09:15' },
+    { startTime: '09:30', endTime: '10:15' },
+    { startTime: '10:30', endTime: '11:15' },
+    { startTime: '11:30', endTime: '12:15' },
+    { startTime: '12:30', endTime: '13:15' },
+    { startTime: '13:30', endTime: '14:15' },
+    { startTime: '14:30', endTime: '15:15' },
+    { startTime: '15:30', endTime: '16:15' },
+    { startTime: '16:30', endTime: '17:15' },
+    { startTime: '17:30', endTime: '18:15' },
+];
+
+const subjects = [
+    { courseID: 'BIOL2420', type: 'LEC', day: 'Tuesday', startTime: '08:30', endTime: '10:15', location: 'YIA Room 1' },
+    { courseID: 'MGNT1040', type: 'LEC', day: 'Wednesday', startTime: '14:30', endTime: '16:15', location: 'MMW Room 1' },
+];
+
+// Define colors for each course
+const subjectColors = {
+    BIOL2420: '#FFA07A',
+    MGNT1040: '#87CEFA',
+};
+
 const TimetableTab = () => {
     return (
-        <>
-            <h1>Timetable</h1>
-        </>
+        <div id='homepage-timetable-tab'>
+           <div id='homepage-timetable-up'>
+                <Upperpart />
+           </div>
+           <div id='homepage-timetable-bottom'>
+                <Timetable />
+           </div>
+        </div>
     );
+}
+
+const Upperpart = () => {
+    return(
+        <>
+            <button>Compact mode</button>
+            <button>Relax mode</button>
+            <h2>Export As: </h2>
+            <button>PDF (.pdf)</button>
+            <button>Image (.jpg)</button>
+        </>
+    )
+}
+
+const rowSpanChecker = subjects.map((subject) => {
+    const startIdx = timeSlots.findIndex(
+        (timeSlot) => timeSlot.startTime === subject.startTime
+    );
+    const endIdx = timeSlots.findIndex(
+        (timeSlot) => timeSlot.endTime === subject.endTime
+    );
+
+    return {
+        ...subject,
+        rowSpan: endIdx - startIdx + 1,
+    };
+});
+
+const Timetable = () => {
+    return(
+        <table id="homepage-timetable">
+            <thead>
+                <tr>
+                    <th></th>
+                    {days.map((day) => (<th key={day}>{day}</th>))}
+                </tr>
+            </thead>
+            <tbody>
+                {timeSlots.map((timeSlot) => (
+                    <tr key={timeSlot.startTime}>
+                        <td style={{ background: '#277582', color: 'white' }}>
+                            {timeSlot.startTime}
+                        </td>
+                        {days.map((day) => {
+                            const subject = rowSpanChecker.find(
+                                (subject) =>
+                                    subject.day === day &&
+                                    subject.startTime === timeSlot.startTime
+                            );
+
+                            if (subject) {
+                                return (
+                                    <td
+                                        key={`${day}-${timeSlot.startTime}`}
+                                        style={{background: subjectColors[subject.courseID],}}
+                                        rowSpan={subject.rowSpan}
+                                    >   
+                                        <div>{subject.courseID} [{subject.type}]</div>
+                                        <div>{subject.location}</div>
+                                    </td>
+                                );
+                            } else if (!rowSpanChecker.some((subject) => subject.day === day && subject.startTime <= timeSlot.startTime && subject.endTime >= timeSlot.endTime)) {
+                                return (
+                                    <td key={`${day}-${timeSlot.startTime}`}></td>
+                                );
+                                
+                            }else{
+                                return null;
+                            }
+                        })}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
 }
   
 export default TimetableTab;
