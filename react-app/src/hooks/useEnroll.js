c38c9c5316ed02c8c0abd2eb08a20bfcb690f28d
OsbornChan123 <kw4aosborn@gmail.com>
2023-04-09 03:53:10 +0800

    completed swap function

--- react-app/src/hooks/useEnroll.js
+++ react-app/src/hooks/useEnroll.js
@@ -54,11 +54,66 @@ const useEnroll = () => {
         .then(json => console.log(json));
     }
 
+    const swap = (enrolledCourseID, enrolledTutorialID, cartCourseID, cartTutorialID) => {
+        fetch('http://localhost:3001/enrolledCourse/drop', {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {'Content-type' : 'application/json'},
+            body: JSON.stringify({
+                courseID: enrolledCourseID,
+                tutorialID: enrolledTutorialID
+            })
+        })
+        .then(res => res.json())
+        .then(json => {
+            //console.log(json);
+            fetch('http://localhost:3001/enrolledCourse/enroll', {
+                method: 'PUT',
+                credentials: 'include',
+                headers: {'Content-type' : 'application/json'},
+                body: JSON.stringify({
+                    courseID: cartCourseID,
+                    tutorialID: cartTutorialID
+                })
+            })
+            .then(res => res.json())
+            .then(json => {
+                //console.log(json);
+                
+                if(json.success){
+                    fetch('http://localhost:3001/shoppingCart/add', {
+                        method: 'PUT',
+                        credentials: 'include',
+                        headers: {'Content-type' : 'application/json'},
+                        body: JSON.stringify({
+                            courseID: enrolledCourseID,
+                            tutorialID: enrolledTutorialID
+                        })
+                    })
+                    .then(()=> {console.log("Successful to swap")})
+                }else{
+                    fetch('http://localhost:3001/enrolledCourse/enroll', {
+                        method: 'PUT',
+                        credentials: 'include',
+                        headers: {'Content-type' : 'application/json'},
+                        body: JSON.stringify({
+                            courseID: enrolledCourseID,
+                            tutorialID: enrolledTutorialID
+                        })
+                    })
+                    .then(()=> {console.log("Failed to swap")})
+                }
+                
+            });
+        })
+    }
+
     return {
         addToCart,
         removeFromCart,
         enroll,
-        drop
+        drop,
+        swap
     }
 }
 
--- react-app/src/pages/osborn.js
+++ react-app/src/pages/osborn.js
@@ -1,15 +1,16 @@
 import useEnroll from '../hooks/useEnroll';
 
 const Osborn = () => {
-    const { addToCart, removeFromCart, enroll,drop} = useEnroll()
+    const { addToCart, removeFromCart, enroll,drop, swap} = useEnroll()
 
     return(
         <div id="login">
             <div className="container">
-                <button onClick={() => addToCart("MGNT3690", "T01")}>Add</button>
+                <button onClick={() => addToCart("MUSC2340")}>Add</button>
                 <button onClick={() => removeFromCart("MGNT3690")}>Remove</button>
-                <button onClick={() => enroll("MGNT3690", "T01")}>Enroll</button>
+                <button onClick={() => enroll("MUSC2340")}>Enroll</button>
                 <button onClick={() => drop("MGNT3690", "T01")}>Drop</button>
+                <button onClick={() => swap("ARCH2180", "T02", "MUSC2340")}>Swap</button>
             </div>
         </div>
     )
