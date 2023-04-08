a59f9a731d75590b577e7eb5085570726bdf1738
OsbornChan123 <kw4aosborn@gmail.com>
2023-04-08 17:13:40 +0800

    fixed addToCart bug and create removeFromCart

--- react-app/src/hooks/useEnroll.js
+++ react-app/src/hooks/useEnroll.js
@@ -13,6 +13,19 @@ const useEnroll = () => {
         .then(json => console.log(json));
     }
 
+    const removeFromCart = (courseID) => {
+        fetch('http://localhost:3001/shoppingCart/remove', {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {'Content-type' : 'application/json'},
+            body: JSON.stringify({
+                courseID: courseID,
+            })
+        })
+        .then(res => res.json())
+        .then(json => console.log(json));
+    }
+
     const enroll = (courseID) => {
         fetch('http://localhost:3001/enrolledCourse/add', {
             method: 'PUT',
@@ -28,6 +41,7 @@ const useEnroll = () => {
 
     return {
         addToCart,
+        removeFromCart,
         enroll
     }
 }
--- server/Routes/courseRoutes.js
+++ server/Routes/courseRoutes.js
@@ -32,18 +32,42 @@ router.put('/shoppingCart/add', (req,res)=>{
     const {courseID, tutorialID} = req.body;
     const username = verifyToken(req.cookies.jwt)
 
-    User.findOne({shoppingCartCourse: {$elemMatch: {courseID, tutorialID}}})
-    .then(existCourse =>{
-        if(existCourse){
+    User.findOne({username})
+    .then(user => {
+        const found = user.shoppingCartCourse.some(el => el.courseID === courseID)
+        if(found){
             return res.status(400).send({success: false, error: "Course already added to shopping cart"})
-        }
-        User.updateOne({ username }, {$push: { shoppingCartCourse: {courseID, tutorialID}}})
+        }else{
+            User.updateOne({ username }, {$push: { shoppingCartCourse: {courseID, tutorialID}}})
         .then(()=> {
             res.send({success: true, error: 'Added to shopping cart'})
         })
+        }
     })
     .catch(error => res.json({error}))
-    
+})
+
+router.put('/shoppingCart/remove', (req,res)=>{
+    const {courseID} = req.body;
+    const username = verifyToken(req.cookies.jwt)
+
+    User.findOne({username})
+    .then(user => {
+        const found = user.shoppingCartCourse.some(el => el.courseID === courseID)
+        if(!found){
+            return res.status(400).send({success: false, error: "Course not found in your shopping cart"})
+        }else{
+            User.updateOne(
+                {username},
+                {$pull : {shoppingCartCourse : {courseID : courseID}}}
+            )
+            .then(() => {
+                res.send({success: true, error: 'Course removed from shopping cart'})
+            })
+            .catch(error => res.json(error))
+        }
+    })
+    .catch(error => res.json({error}))
 })
 
 router.put('/enrolledCourse/enroll', (req,res)=>{
