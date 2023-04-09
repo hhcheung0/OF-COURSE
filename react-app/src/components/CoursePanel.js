7f77271c5c54ed46e83550d2e2ccb2c874811ec9
Hinson <newspaperhinson@gmail.com>
2023-04-09 23:39:09 +0800

    new CoursePanel

--- react-app/src/components/CoursePanel.js
+++ react-app/src/components/CoursePanel.js
@@ -17,7 +17,7 @@ const CoursePanel = () => {
 
     return (
         <div id="admin-course">
-            <div class="row">
+            <div className="row">
 {/* courseSearchbar */}
                 {/* <h3 id="courseSearchbar">Search <input type="text"></input></h3> */}
                 <div id='table-panel'>
@@ -52,9 +52,9 @@ const CoursePanel = () => {
                 </table>
             </div> */}
             
-            <div class="row">
+            <div className="row">
 {/* courseForm */}                    
-                    <div class="container" id="courseForm">
+                    <div className="container" id="courseForm">
                         <CourseForm course={course}/>
                         {/* <form method="post"> */}
 {/* Left side of the form - Lecture*/}
@@ -144,8 +144,8 @@ const CoursePanel = () => {
                     </div>
             </div>
 
-            <div class="row">
-                <div class="column">
+            <div className="row">
+                <div className="column">
 {/* courseComment */}
                     <table id="courseComment">
                         <thead>
@@ -171,7 +171,7 @@ const CoursePanel = () => {
                     </table>
                 </div>
 {/* courseCommentAdd */}
-                <div class="column" id="courseCommentAdd">
+                <div className="column" id="courseCommentAdd">
                     <h3>To Add Comment:</h3>
                     <br/>
                     <button><Link to="/course">Course Browsing Page</Link></button>
@@ -207,7 +207,7 @@ const SearchBar = (props) => {
 const CourseTable = ({courseArray, controller}) => {
 
     return (
-        <div class="row" id='table-container'>
+        <div className="row" id='table-container'>
             <table id='course-table'>
                 <thead>
                     <tr>
@@ -232,7 +232,6 @@ const CourseTable = ({courseArray, controller}) => {
 
 const CourseTableRow = ({course, controller}) => {
     const { parseTimecodeArray } = useTime()
-
 
     return (
         <>
@@ -311,94 +310,124 @@ const CourseTableRow = ({course, controller}) => {
 
 const CourseForm = ({course}) => { // state
     const { parseTimecodeArray } = useTime()
-    const [ tutorial, setTutorial] = useState({}); // state, function to adjust the state
-    const [ courseName, setCourseName] = useState("");
+    const [ tutorial, setTutorial ] = useState({}); // state, function to adjust the state
+    const [ courseName, setCourseName ] = useState('')
+    const [ tutorialIndex, setTutorialIndex ] = useState(null)
+    const [ tutorialID, setTutorialID ] = useState('')
+    const [ tutorialFormNumber, setTutorialFormNubmer ] = useState([])
     console.log(course);
     console.log(course.tutorialInfo);
+
+    const handleCourseNameChange = (e) => {
+        console.log(e.target.id)
+        setCourseName(e.target.value)
+    }
+
+    useEffect(() => {
+        setCourseName(course.courseName)
+        if (!course.tutorialInfo) return
+        setTutorialFormNubmer([...new Array(course.tutorialInfo.length).keys()])
+    }, [course])
+
+    useEffect(() => {
+        console.log(tutorialFormNumber)
+    }, [tutorialFormNumber])
+
+    useEffect(() => {
+        if (!course.tutorialInfo) return
+        setTutorialID(course.tutorialInfo[Number(tutorialIndex)].tutorialID)
+    }, [tutorialIndex, setTutorialID])
+
+    const handleTutorialIndexChange = (e) => {
+        setTutorialIndex(e.target.value)
+    }
 
     return(
         <>
         {/* {course && */}
                     <form method="post">
 {/* Left side of the form - Lecture*/}
-                        <div class="column" id="left">
+                        <div className="column" id="left">
                         <h3>Lecture</h3>
-                            <label for="courseID">Course ID</label>
+                            <label htmlFor="courseID">Course ID</label>
                             <input type="text" id="courseID" name="courseID" value={course.courseID} disabled/>
                             <br/>
 
-                            <label for="courseName">Course Name</label>
-                            <input type="text" id="courseName" name="courseName" value={courseName}/>
+                            <label htmlFor="courseName">Course Name</label>
+                            <input type="text" id="courseName" name="courseName" value={courseName} onChange={handleCourseNameChange}/>
                             <br/>
 
-                            <label for="courseTime">Time</label>
-                            <input type="text" id="courseTime" name="courseTime" value={parseTimecodeArray(course.courseTime).join(', ')}/> 
+                            <label htmlFor="courseTime">Time</label>
+                            <input type="text" id="courseTime" name="courseTime" value={course.courseTime}/> 
                             <br/>
                             
-                            <label for="courseLocation">Location</label>
+                            <label htmlFor="courseLocation">Location</label>
                             <input type="text" id="courseLocation" name="courseLocation" value={course.courseLocation}/>
                             <br/>
 
-                            <label for="instructor">Instructor</label>
+                            <label htmlFor="instructor">Instructor</label>
                             <input type="text" id="instructor" name="instructor" value={course.instructor}/>
                             <br/>
 
-                            <label for="department">Department</label>
+                            <label htmlFor="department">Department</label>
                             <input type="text" id="department" name="department" value={course.department}/>
                             <br/>
 
-                            <label for="courseCapacity">Capacity</label>
+                            <label htmlFor="courseCapacity">Capacity</label>
                             <input type="text" id="courseCapacity" name="courseCapacity" value={course.courseLocation}/>
                             <br/>
 
-                            <label for="prerequisiteCourseID">Pre-requisite Course(s)</label>
+                            <label htmlFor="prerequisiteCourseID">Pre-requisite Course(s)</label>
                             <input type="text" id="prerequisiteCourseID" name="prerequisiteCourseID" value={changeSlash(course.prerequisiteCourseID)}/>
                             <br/>
 
-                            <label for="forbiddenCourseID">Forbidden Course(s)</label>
+                            <label htmlFor="forbiddenCourseID">Forbidden Course(s)</label>
                             <input type="text" id="forbiddenCourseID" name="forbiddenCourseID" value={changeSlash(course.forbiddenCourseID)}/>
                             <br/>
 
-                            <label for="credit">Credit</label>
+                            <label htmlFor="credit">Credit</label>
                             <input type="text" id="credit" name="credit" value={course.credit}/>
                             <br/>
 
-                            <label for="outline">Outline</label>
+                            <label htmlFor="outline">Outline</label>
                             <input type="text" id="outline" name="outline" value={course.outline}/>
                             <br/>
                         </div>
 {/* Right side of the form - Tutorial */}
-                        <div class="column" id="right">
+                        <div className="column" id="right">
                         <h3>Tutorial 
-                            <select name="tutNo" id="tutNo">
-                            <option value="one">1</option>  
-                            <option value="two">2</option>
-                            <option value="three">3</option>
+                            <select name="tutNo" id="tutNo" onChange={handleTutorialIndexChange}>
+                            {tutorialFormNumber && tutorialFormNumber.map((number, idx) => (
+                                <option value="" key={idx}>{number+1}</option>
+                            ))}
+                            {/* <option value={0}>1</option>  
+                            <option value={1}>2</option>
+                            <option value={2}>3</option> */}
                             </select> 
                         </h3>
-                            <label for="tutid">Tutorial ID</label>
-                            <input type="text" id="tutid" name="tutid"/>
+                            <label htmlFor="tutid">Tutorial ID</label>
+                            <input type="text" id="tutid" name="tutid" value={tutorialID}/>
                             <br/>
 
-                            <label for="tuttime">Time</label>
+                            <label htmlFor="tuttime">Time</label>
                             <input type="text" id="tuttime" name="tuttime"/>
                             <br/>
                             
-                            <label for="tutloc">Location</label>
+                            <label htmlFor="tutloc">Location</label>
                             <input type="text" id="tutloc" name="tutloc"/>
                             <br/>
 
-                            <label for="tutor">Tutor</label>
+                            <label htmlFor="tutor">Tutor</label>
                             <input type="text" id="tutor" name="tutor"/>
                             <br/>
 
-                            <label for="tutcap">Capacity</label>
+                            <label htmlFor="tutcap">Capacity</label>
                             <input type="text" id="tutcap" name="tutcap"/>
                             <br/>
                         {/* <input type="submit" class="btn1" value="Add/Update" />
                         <input type="submit" class="btn2" value="Clear" /> */}
-                        <button class="btn1">Add/Update</button>
-                        <button class="btn2">Clear</button>
+                        <button className="btn1">Add/Update</button>
+                        <button className="btn2">Clear</button>
 
                         </div>
                         <br/>
