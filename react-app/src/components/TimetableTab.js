3fd0e4d7c4c6f718684181c24a072c74183bac2f
hhcheung0 <1155144254@link.cuhk.edu.hk>
2023-04-12 12:57:45 +0800

    Added Export image and pdf button

--- react-app/src/components/TimetableTab.js
+++ react-app/src/components/TimetableTab.js
@@ -1,31 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import Timetable from './TimeTable';
+import ExportAsPng from './ExportPng';
+
 
 const TimetableTab = ({courseArray}) => {
+
+    const exportRef = useRef();
+
+    const exportToPdf = async (element, pdfFileName) => {
+        const canvas = await html2canvas(element);
+        const dataURL = canvas.toDataURL();
+        
+        const pdf = new jsPDF();
+        pdf.addImage(dataURL, 'PNG', 5, 10, 200, 100); 
+        pdf.save(pdfFileName); 
+    };
+
+    const Upperpart = () => {
+
+        return(
+            <>
+                <button>Compact mode</button>
+                <button>Relax mode</button>
+                <h2>Export As: </h2>
+                <button onClick={() => exportToPdf(exportRef.current, "timetable")}>PDF (.pdf)</button>
+                <button onClick={() => ExportAsPng(exportRef.current, "timetable")}>Image (.png)</button>
+            </>
+        )
+    }
 
     return (
         <div id='homepage-timetable-tab'>
            <div id='homepage-timetable-up'>
                 <Upperpart />
            </div>
-           <div id='homepage-timetable-bottom'>
+           <div id='homepage-timetable-bottom' ref={exportRef}>
                 {/* <Timetable /> */}
                 <Timetable courseArray={courseArray} />
            </div>
         </div>
     );
+
+    
 }
 
-const Upperpart = () => {
-    return(
-        <>
-            <button>Compact mode</button>
-            <button>Relax mode</button>
-            <h2>Export As: </h2>
-            <button>PDF (.pdf)</button>
-            <button>Image (.jpg)</button>
-        </>
-    )
-}
+
   
 export default TimetableTab;
