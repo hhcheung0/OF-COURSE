2af1142491b3f2f39479930e934cec263f626214
OsbornChan123 <kw4aosborn@gmail.com>
2023-04-12 15:20:44 +0800

    create hooks to get enrolled & completed credit

--- react-app/src/pages/osborn.js
+++ react-app/src/pages/osborn.js
@@ -1,9 +1,13 @@
+import React, { useState, useEffect } from "react"
 import useEnroll from '../hooks/useEnroll';
 import useComment from '../hooks/useComment';
+import useUser from '../hooks/useUser'
 
 const Osborn = () => {
-    const { addToCart, removeFromCart, enroll,drop, swap} = useEnroll()
+    const { addToCart, removeFromCart, enroll,drop, swap, getEnrolledCredit, getCompletedCredit} = useEnroll()
     const { removeComment} = useComment();
+    const { getUserByToken } = useUser()
+
     return(
         <div id="login">
             <div className="container">
@@ -13,6 +17,8 @@ const Osborn = () => {
                 <button onClick={() => drop("MGNT3690", "T01")}>Drop</button>
                 <button onClick={() => swap("ARCH1290", "T01", "ARCH1290","T02")}>Swap</button>
                 <button onClick={() => removeComment("HIST1890","Testing")}>Remove Comment</button>
+                <button onClick={() => getEnrolledCredit()}>Get enrolled credit</button>
+                <button onClick={() => getCompletedCredit()}>Get completed credit</button>
             </div>
         </div>
     )
