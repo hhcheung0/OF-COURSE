3ea028257c8a3782dbd41ba42d46a0f3bc938dd8
hhcheung0 <1155144254@link.cuhk.edu.hk>
2023-04-27 13:45:55 +0800

    Added comments in ExportPng and TimetableTab

--- react-app/src/components/ExportPng.js
+++ react-app/src/components/ExportPng.js
@@ -1,22 +1,36 @@
 import html2canvas from "html2canvas";
 
+// Export given HTML element as a PNG image with given filename
 const ExportAsPng = async (element, imageFileName) => {
+
+    // Use html2canvas to create a canvas element from the given element
     const canvas = await html2canvas(element);
+
+    // Convert the canvas element to a data URL representing a PNG image
     const image = canvas.toDataURL("image/png", 1.0);
+
+    // Call downloadImage function with image data URL and filename parameters
     downloadImage(image, imageFileName);
 };
 
+// Download given blob as a file with given filename
 const downloadImage = (blob, fileName) => {
+
+    // Create a new hidden a element
     const hiddenLink = window.document.createElement("a");
     hiddenLink.style = "display:none;";
+
+    // Set the download attribute to the given filename
     hiddenLink.download = fileName;
 
     hiddenLink.href = blob;
-
     document.body.appendChild(hiddenLink); 
     hiddenLink.click(); 
+
+    // Remove the link from the document body
     document.body.removeChild(hiddenLink);
 
+    // Remove the link from memory
     hiddenLink.remove(); 
 };
 
--- react-app/src/components/TimetableTab.js
+++ react-app/src/components/TimetableTab.js
@@ -6,17 +6,27 @@ import ExportAsPng from './ExportPng';
 
 const TimetableTab = ({ courseArray }) => {
 
+    // Create a reference for the element to be exported
     const exportRef = useRef();
 
+    // Export the given element as a PDF with the given filename
     const exportToPdf = async (element, pdfFileName) => {
+
+        // Use html2canvas to create a canvas element from the given element
         const canvas = await html2canvas(element);
         const dataURL = canvas.toDataURL();
 
+        // Create a new jsPDF object
         const pdf = new jsPDF();
+
+        // Add the image to the PDF object
         pdf.addImage(dataURL, 'PNG', 5, 10, 200, 100);
+
+        // Download the PDF with the given filename
         pdf.save(pdfFileName);
     };
 
+    // For the upper part of the timetable tab
     const Upperpart = () => {
         return (
             <>
@@ -41,7 +51,5 @@ const TimetableTab = ({ courseArray }) => {
 
 
 }
-
-
 
 export default TimetableTab;
